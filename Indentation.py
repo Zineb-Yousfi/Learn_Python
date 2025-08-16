24f167826c229dee11f3f6995a1263e61a8c349d
Zineb-Yousfi <yousfi.zineb.yz@gmail.com>
2025-08-16 08:03:12 +0100

    Indentation

---
+++ Indentation.py
@@ -0,0 +1,6 @@
+# Indentation
+# Python uses indentation for blocks, instead of curly braces. Both tabs and spaces are supported, but the standard indentation requires standard Python code to use four spaces. For example:
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
