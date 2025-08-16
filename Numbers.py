6b213f0489694f3b1f6b06698a47a378edbe6f8b
Zineb-Yousfi <yousfi.zineb.yz@gmail.com>
2025-08-16 08:07:29 +0100

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+# Variables and Types
+# Python is completely object oriented, and not "statically typed". You do not need to declare variables before using them, or declare their type. Every variable in Python is an object.
+
+# This tutorial will go over a few basic types of variables.
+
+# Numbers
+# Python supports two types of numbers - integers(whole numbers) and floating point numbers(decimals). (It also supports complex numbers, which will not be explained in this tutorial).
+
+# To define an integer, use the following syntax:
+myint = 7
+print(myint)
+
+# To define a floating point number, you may use one of the following notations:
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
